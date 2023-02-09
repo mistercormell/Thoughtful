0143393aa06b5962b45b897db8b65fefa4dfc48c
David Cormell <d.cormell@etoncollege.org.uk>
2023-02-09 13:40:01 +0000

    Initial Commit

---
+++ Thoughtful/ThoughtfulApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThoughtfulApp.swift
+//  Thoughtful
+//
+//  Created by Cormell, David - DPC on 09/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct ThoughtfulApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
